2c233e8516fa6a27f6229735804b3ad156779d95
Rafiul Kabir <214873992+RafiulKabir0@users.noreply.github.com>
2026-07-11 15:39:56 +0600

    Fix CORS for Netify

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_FILE = path.join(__dirname, 'db.json');
 
-app.use(cors());
+app.use(cors({
+  origin: "https://rafiulkabir-portfolio.netlify.app",
+  methods: ["GET", "POST", "PUT"],
+  credentials: true,
+}));
 app.use(express.json({ limit: '50mb' }));
 
 // Helper to read DB
